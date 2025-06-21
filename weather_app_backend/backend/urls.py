5b2425be98e602fed252b3029b15769770ff081b
Szymon <ssikora258@gmail.com>
2025-06-21 22:33:06 +0200

    forecast endpoint first iteration

--- weather_app_backend/backend/urls.py
+++ weather_app_backend/backend/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('forecast/', views.We)
+    path('forecast/', views.WeatherForecastView.as_View(), name='forecast'),
+    
 ]
--- weather_app_backend/backend/views.py
+++ weather_app_backend/backend/views.py
@@ -50,4 +50,64 @@ class WeatherForecastView(APIView):
             response = requests.get(url, params)
 
 
+            if response.status_code != 200:
+                return Response ({
+                    'error': 'faild to fetch data from open-meteo'
+                }, status=status.HTTP_502_BAD_GATEWAY)
             
+            weather_data = response.json()
+            
+            if 'daily' not in weather_data:
+                return Response({
+                    'error': 'invalid response from API'
+                }, status=status.HTTP_502_BAD_GATEWAY)
+            
+            #obliczanie szacowanej wygenerowanej energii w kWh
+
+            SOLAR_FARM_POWER_KW = 2.5 #moc instalacji w kW
+            FARM_EFFICIENCY = 0.2 #sprawność instalacji
+
+            #przetworzenie danych w celu uzyskania prognozy na 7 dni
+            forecast_data = []
+            daily_data = weather_data['daily']
+
+            for i in range(7):
+                date = daily_data['time'][i]
+                weather_code = daily_data['weathercode'][i]
+                temp_max = daily_data['temperature_2m_max'][i]
+                temp_min = daily_data['temperature_2m_min'][i]
+                sunshine_time_seconds = daily_data['sunshine_duration'][i] #api zwraca w sekundach
+
+                sunshine_time_hours = sunshine_time_seconds / 3600
+
+                #obliczenie enegrii wygenerowanej
+                #jak w treści zadania
+                #energy[kWh] = moc[kW] x czas[h] x sprawność
+                energy_generated = SOLAR_FARM_POWER_KW * sunshine_time_hours * FARM_EFFICIENCY
+
+                forecast_data.append({
+                    'date': date,
+                    'weather_code': weather_code,
+                    'temperature_max': temp_max,
+                    'temperature_min': temp_min,
+                    'gen_energry_kWh': energy_generated,
+                    'sunshine_time_hours': round(sunshine_time_hours,2)
+                })
+
+            return Response({
+                'forecast': forecast_data,
+                'location': {
+                    'latitude': lat,
+                    'longitude': lon
+                }
+            })
+
+        except requests.RequestExcetption:
+            return Response ({
+                'error': 'failed connection to weather servie'
+            }, status=status.HTTP_503_SERVICE_UNAVAILABLE)
+        
+        except Exception as e:
+            return Response({
+                'error': 'Internal server error'
+            }, status=status.status.HTTP_500_INTERNAL_SERVER_ERROR)
